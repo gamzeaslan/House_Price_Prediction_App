2028e0b2654f4230df8acbd29c47257cdcbda844
Gamze Aslan <95714545+gamzeaslan@users.noreply.github.com>
2023-04-30 20:53:25 +0300

    Add files via upload

---
+++ Module.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def ExterQual_TA(data):
+    if ( data == "ExterQual_TA"):
+        return (1)
+    else:
+        return (0)
+    
+def ExterQual_Ex(data):
+    if ( data == "ExterQual_Ex"):
+        return (1)
+    else:
+        return (0)
+
+
+# In[2]:
+
+
+def BsmtQual_TA(data):
+    if ( data == "BsmtQual_TA"):
+        return (1)
+    else:
+        return (0)
+    
+def BsmtQual_Ex(data):
+    if ( data == "BsmtQual_Ex"):
+        return (1)
+    else:
+        return (0)
+
+
+# In[3]:
+
+
+def KitchenQual_TA(data):
+    if ( data == "KitchenQual_TA"):
+        return (1)
+    else:
+        return (0)
+
+def KitchenQual_Ex(data):
+    if ( data == "KitchenQual_Ex"):
+        return (1)
+    else:
+        return (0)
+
